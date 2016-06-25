f6e00b508dec5754cd49282f816d1de40c2c29c0
Dustin Van Tate Testa <toast27@gmail.com>
2016-06-25 11:55:36 -0400

    this code is all over the place...

---
+++ wifi-fish-feeder.ino
@@ -0,0 +1,63 @@
+
+#include "Arduino.h"
+#include <Servo.h> 
+#include <inttypes.h>
+#include <limits.h>
+
+#define MILISECONDS_PER_DAY 86400000
+
+#define SSID "tates telefone"
+#define PASSWORD "jkjk"
+
+#include "network.h"
+
+Servo feedServo; 
+
+
+void feedFish(uint8_t& slotNum, int8_t initialPosition = -180){
+  // 360 / 9 = 40 degrees
+  feedServo.write(40 * slotNum);
+  delay(500);
+
+  feedServo.write(initialPosition);
+
+  slotNum++;
+  
+}
+
+
+void setup(){
+  feedServo.attach(A3); 
+  feedServo.write(-180);
+
+  Serial.begin(9600);
+  Serial.println(sizeof(unsigned long));
+  
+} 
+
+
+void loop(){ 
+  
+  static uint32_t msRemaining = MILISECONDS_PER_DAY;
+  static uint8_t slot = 0;
+
+  // check the user input from the internet
+  if (checkInput()) {
+    feedFish(slot);
+    msRemaining = MILISECONDS_PER_DAY;
+    return;    
+  }
+  
+  
+  // wait 
+  delay(5);  
+  msRemaining -= 5;
+
+  
+  if (msRemaining == 0) {
+    feedFish(slot);
+    msRemaining = MILISECONDS_PER_DAY;
+    return; 
+  }
+  
+}
